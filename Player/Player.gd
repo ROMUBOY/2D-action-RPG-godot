05a01932d86a8c6ab143e0838a047d085c9d498c
Romualdo "Romuboy" Santos <romualdoosfilho94@gmail.com>
2024-06-25 09:32:31 -0300

    4 directions animation tree

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,15 +3,21 @@ extends CharacterBody2D
 @export var max_speed = 80
 @export var acceleration = 500
 @export var friction = 500
-
+@onready var animation_player = $AnimationPlayer
+@onready var animation_tree = $AnimationTree
+@onready var animationState = animation_tree.get("parameters/playback")
 func _physics_process(delta):
 	var input_vector = Input.get_vector("move_left","move_right","move_up","move_down")
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity = velocity.move_toward(input_vector * max_speed, acceleration * delta)
+		animation_tree.set("parameters/Idle/blend_position", input_vector)
+		animation_tree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		
+		velocity = velocity.move_toward(input_vector * max_speed, acceleration * delta)		
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)	
 	
 	move_and_slide()

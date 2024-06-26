2970db4599e0950514b6b50f7ee3b1d35de145f7
Romualdo "Romuboy" Santos <romualdoosfilho94@gmail.com>
2024-06-26 09:57:31 -0300

    attack animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -7,9 +7,26 @@ extends CharacterBody2D
 @onready var animation_tree = $AnimationTree
 @onready var animationState = animation_tree.get("parameters/playback")
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
+
 var motion = Vector2.ZERO
 
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			pass
+		ATTACK:
+			attack_state(delta)
+
+func move_state(delta):
 	motion_mode = 1
 	var input_vector = Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down"))
 	input_vector = input_vector.normalized()
@@ -17,6 +34,7 @@ func _physics_process(delta):
 	if input_vector != Vector2.ZERO:
 		animation_tree.set("parameters/Idle/blend_position", input_vector)
 		animation_tree.set("parameters/Run/blend_position", input_vector)
+		animation_tree.set("parameters/Attack/blend_position", input_vector)
 		animationState.travel("Run")
 		
 		motion = motion.move_toward(input_vector * max_speed, acceleration * delta)
@@ -27,3 +45,13 @@ func _physics_process(delta):
 	velocity = motion
 	move_and_slide()
 	motion = velocity
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+
+func attack_state(delta):
+	velocity = Vector2.ZERO
+	animationState.travel("Attack")
+
+func attack_animation_finished():
+	state = MOVE
